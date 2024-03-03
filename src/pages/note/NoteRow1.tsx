64588637cf037ed49abe57a001d5fef088db5230
liuyang <flow_ocean@yahoo.com>
2024-03-03 12:22:57 +0800

    progress

--- src/pages/note/NoteRow1.tsx
+++ src/pages/note/NoteRow1.tsx
@@ -1,4 +1,4 @@
-import {Box, Button, Chip, Grid, Paper} from "@mui/material";
+import {Box, Button, Chip, Grid, Paper, Stack} from "@mui/material";
 import {useTranslation} from "react-i18next";
 import {useDispatch, useSelector} from "react-redux";
 import {useNavigate} from "react-router-dom";
@@ -11,18 +11,36 @@ const NoteRow1 = (data: any) => {
     const {t} = useTranslation();
     const dispatch = useDispatch()
     const theme = useTheme()
+    console.log(row)
 
     return (
         <Paper sx={{marginTop: 1, maxWidth: 1080, background: theme.palette.background.default}}>
-            <Button variant='outlined' sx={{width: '100%'}} onClick={() => {
-                dispatch(saveNoteId(row.noteId))
-                // navigate('/NoteEdit')
-                navigate('/NoteDetail')
-            }}>
+            <div style={{}}>
+                <Button sx={{width: '100%'}} onClick={() => {
+                    dispatch(saveNoteId(row.noteId))
+                    // navigate('/NoteEdit')
+                    navigate('/NoteDetail')
+                }}>
                 <span style={{color: theme.palette.primary.main}}>
                     {row?.title ? row.title : 'no title'}
                 </span>
-            </Button>
+                </Button>
+                {row && row.tagList && row.tagList.length > 0 ?
+                    <div style={{display: "flex", justifyContent: "center", padding: 5}}>
+                        <Stack direction='row' spacing={1}>
+                            {
+                                row.tagList.map((item: any, index: any) => (
+                                    <Chip size='small'
+                                          label={item.tagName} key={index}/>
+                                ))
+                            }
+                        </Stack>
+                    </div>
+                    :
+                    null
+                }
+            </div>
+
         </Paper>
     )
 }
